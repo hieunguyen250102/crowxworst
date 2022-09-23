bee7e18ff9338788158b1ec1e0faf8b6c310581d
hieunguyen250102 <108143051+hieunguyen250102@users.noreply.github.com>
2022-09-23 13:28:30 +0700

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,7 +1,7 @@
 <?php 
 function getConnection()
 {
-    $dbHost = ("mysql:host=localhost; dbname=oI1hnpf6R7");
+    $dbHost = ("mysql:host=remotemysql.com; dbname=oI1hnpf6R7");
     $dbUser = "oI1hnpf6R7";
     $dbPass = "7AaGzD53vE";
     $conn = new PDO($dbHost, $dbUser, $dbPass);
