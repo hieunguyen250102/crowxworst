9c628da3cb147490972ec2ff8ac5011cb4ef31c8
hieunguyen250102 <108143051+hieunguyen250102@users.noreply.github.com>
2022-09-23 14:09:21 +0700

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,9 +1,9 @@
 <?php 
 function getConnection()
 {
-    $dbHost = ("mysql:host=remotemysql.com; dbname=oI1hnpf6R7");
-    $dbUser = "oI1hnpf6R7";
-    $dbPass = "7AaGzD53vE";
+    $dbHost = ("mysql:host=sql302.epizy.com; dbname=epiz_32651517");
+    $dbUser = "epiz_32651517";
+    $dbPass = "H6w2xpE5nwHs4";
     $conn = new PDO($dbHost, $dbUser, $dbPass);
     
     return $conn; 
